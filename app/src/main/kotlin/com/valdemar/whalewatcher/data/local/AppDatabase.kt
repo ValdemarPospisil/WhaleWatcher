a15e12e1c5ecf849150ca372f4b5a7167f6d84be
ValdemarPospisil <valda.pospisil.02@gmail.com>
2026-02-25 09:38:03 +0100

    Update AppDatabase.kt, ImageDao.kt, DockerImageEntity.kt, and 3 more files

--- app/src/main/kotlin/com/valdemar/whalewatcher/data/local/AppDatabase.kt
+++ app/src/main/kotlin/com/valdemar/whalewatcher/data/local/AppDatabase.kt
@@ -3,9 +3,13 @@ package com.valdemar.whalewatcher.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.valdemar.whalewatcher.data.local.dao.ImageDao
-import com.valdemar.whalewatcher.data.local.entities.ImageEntity
+import com.valdemar.whalewatcher.data.local.entities.DockerImageEntity
 
-@Database(entities = [ImageEntity::class], version = 1)
+@Database(entities = [DockerImageEntity::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun imageDao(): ImageDao
+
+    companion object {
+        const val DATABASE_NAME = "whalewatcher.db"
+    }
 }
