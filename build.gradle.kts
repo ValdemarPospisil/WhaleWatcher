6e110972be2cf940595d6942a73d8d59239c820d
Valdemar Pospíšil <valdemar.pospisil@goodaccess.com>
2026-06-29 09:51:41 +0200

    ci: add Kover plugin and expand CI workflow to include linting, testing, coverage reporting, and APK artifact upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.kover) apply false
 }
